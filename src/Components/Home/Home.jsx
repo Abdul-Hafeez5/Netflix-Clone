4eb6f0e041d54aeb133a550295cbc5e091d4e397
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-01-06 23:25:32 +0500

    Update home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./Home.scss";
 import Row from "./Row";
 import axios from "axios";
-import {BiPlay} from "react-icons/bi"
-import {AiOutlinePlus} from "react-icons/ai"
+import { BiPlay } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Api_key = "c7f9c098ad7baa20b53430ceaf490f8a";
 const url = "https://api.themoviedb.org/3";
@@ -12,7 +12,6 @@ const popularMovie = "/movie/popular?";
 const upcomingMovie = `https://api.themoviedb.org/3/movie/upcoming?api_key=${Api_key}`;
 const tvShowsMovies = `https://api.themoviedb.org/3/discover/tv?api_key=${Api_key}`;
 const nowPlayingMovies = `https://api.themoviedb.org/3/movie/now_playing?api_key=${Api_key}`;
-// const genrenames = `https://api.themoviedb.org/3/genre/movie/list?api_key=${Api_key}`;
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -59,10 +58,25 @@ const Home = () => {
         }}
       >
         <h1>Retribution</h1>
-        <p>When a mysterious caller puts a bomb under his car seat, Matt Turner begins a high-speed chase across the city to complete a specific series of tasks. With his kids trapped in the back seat and a bomb that will explode if they get out of the car, a normal commute becomes a twisted game of life or death as Matt follows the stranger's increasingly dangerous instructions in a race against time to save his family.</p>
+
+        <p>
+          When a mysterious caller puts a bomb under his car seat, Matt Turner
+          begins a high-speed chase across the city to complete a specific
+          series of tasks. With his kids trapped in the back seat and a bomb
+          that will explode if they get out of the car, a normal commute becomes
+          a twisted game of life or death as Matt follows the stranger's
+          increasingly dangerous instructions in a race against time to save his
+          family.
+        </p>
         <div>
-        <button> <BiPlay/> Play </button>
-        <button> <AiOutlinePlus/> My List </button>
+          <button>
+            {" "}
+            <BiPlay /> Play{" "}
+          </button>
+          <button>
+            {" "}
+            <AiOutlinePlus /> My List{" "}
+          </button>
         </div>
       </div>
       <Row title={"Popular on Netflix"} movieArray={popularMovies} />
