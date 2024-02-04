ba86ce884f3beebd95c598bae7ffda5b36216b4e
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-02-05 00:59:33 +0500

    header updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,12 @@ const Header = () => {
   return (
     <div>
       <nav className="Header">
-        <img src={"https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"} alt="Logo" />
+        <img
+          src={
+            "https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+          }
+          alt="Logo"
+        />
 
         <div>
           <Link to="/tvshows">TV Shows</Link>
